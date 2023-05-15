a82b14731907befcaff05acd790648a14f555559
OleksandrNovyk <novykav@gmail.com>
2023-05-15 14:10:43 +0300

    1

--- src/components/StartText/StartText.jsx
+++ src/components/StartText/StartText.jsx
@@ -1,12 +1,12 @@
-import { BsEmojiWink } from 'react-icons/bs';
-import { StartTxtWrapper, StartTxt } from './StartText.styled';
+
+import { StartTxtWrapper } from './StartText.styled';
 
 export const StartText = () => {
   return (
     <StartTxtWrapper>
       {/* <StartTxt>
         Tons of pics are waiting for you!
-        <BsEmojiWink />
+        
       </StartTxt> */}
       {/* <StartTxt>Let's start!</StartTxt> */}
     </StartTxtWrapper>
