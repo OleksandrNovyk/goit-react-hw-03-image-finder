892a1e48f500c4df98d6deea6f4d23fdb2b58d96
OleksandrNovyk <novykav@gmail.com>
2023-05-15 14:00:09 +0300

    1

--- src/components/StartText/StartText.jsx
+++ src/components/StartText/StartText.jsx
@@ -4,11 +4,11 @@ import { StartTxtWrapper, StartTxt } from './StartText.styled';
 export const StartText = () => {
   return (
     <StartTxtWrapper>
-      <StartTxt>
+      {/* <StartTxt>
         Tons of pics are waiting for you!
         <BsEmojiWink />
-      </StartTxt>
-      <StartTxt>Let's start searching!</StartTxt>
+      </StartTxt> */}
+      {/* <StartTxt>Let's start!</StartTxt> */}
     </StartTxtWrapper>
   );
 };
